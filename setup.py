e8f499a7b763e0993196bd2d42ba95e6a183526e
steffen <stro889002@aol.com>
2023-02-04 16:08:59 +0100

    refactor: add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='patbefrag',
+    version='',
+    packages=['window', 'ui_form', 'database', 'checkboxdict'],
+    url='',
+    license='',
+    author='steffen',
+    author_email='',
+    description=''
+)

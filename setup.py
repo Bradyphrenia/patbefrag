362e5df8f4db701ebf711516b89c79038146719f
steffen <stro889002@aol.com>
2023-02-16 20:13:38 +0100

    refactor: dictionary comprehension

--- setup.py
+++ setup.py
@@ -1,9 +1,37 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+from setuptools.extension import Extension
+
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
 
 setup(
     name='patbefrag',
-    version='',
+    version='0.100',
     packages=['window', 'ui_form', 'database', 'checkboxdict'],
+    ext_modules=cythonize(
+        [
+            Extension("checkboxdict",
+                      ["checkboxdict/checkboxdict.py"]),
+            Extension("database",
+                      ["database/database.py"]),
+            Extension("datadict",
+                      ["datadict/datadict.py"]),
+            Extension("datafielddict",
+                      ["datafielddict/datafielddict.py"]),
+            Extension("ui_form",
+                      ["ui_form/ui_form.py"]),
+            Extension("window",
+                      ["window/window.py"]),
+        ],
+        build_dir="build_cythonize",
+        compiler_directives={
+            'language_level': "3",
+            'always_allow_keywords': True,
+        }
+    ),
+    cmdclass=dict(
+        build_ext=build_ext
+    ),
     url='',
     license='',
     author='steffen',

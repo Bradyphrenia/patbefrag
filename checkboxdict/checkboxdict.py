50ede41ca06fea35fafe9bdb4bfda49633e074ad
steffen <stro889002@aol.com>
2023-03-01 13:46:06 +0100

    refactor:  CheckBoxDict- methods (suggested by KingKaos)

--- checkboxdict/checkboxdict.py
+++ checkboxdict/checkboxdict.py
@@ -11,20 +11,17 @@ class CheckBoxDict(dict):
         if position not in self.keys():
             self[position] = ckb
 
-    def check(self, position):
-        if 0 <= position < len(self):
-            self[position].setChecked(True)
-            for c in self:
-                if c != position:
-                    self[c].setChecked(False)
+    def check(self, value):
+        for val in self.values():
+            val.setChecked(True) if val == value else val.setChecked(False)
 
-    def bind(self, position):
-        if 0 <= position < len(self):
-            return self[position].clicked.connect(lambda: self.check(position))
+    def bind(self, value):
+        if value in self.values():
+            return value.clicked.connect(lambda: self.check(value))
 
     def bindall(self):
-        for c, _ in enumerate(self):
-            self.bind(c)
+        for val in self.values():
+            self.bind(val)
 
     @property
     def note(self):
